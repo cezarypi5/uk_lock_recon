01513eeb99d04ca1764d1258d7e06080e0ad9caa
cezarypi5 <c.makulec@cmelectronics.co.uk>
2026-03-05 20:26:12 +0000

    v1.0.1 Firebase configuration alignment

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getFirestore, collection, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
-// The configuration generated for 'Lock Recon Dashboard'
+// The configuration exacted from the authentic Firebase Web App
 const firebaseConfig = {
-    apiKey: "AIzaSyBNdQxrLWz4eTXspHVAQzNBe-kZxw5iqKc",
-    authDomain: "lock-recon.firebaseapp.com",
-    projectId: "lock-recon",
-    storageBucket: "lock-recon.firebasestorage.app",
-    messagingSenderId: "482615452902",
-    appId: "1:482615452902:web:db5274b580a814b46d0727"
+    apiKey: "AIzaSyDaMOolKyaTb7eFv2Avrz_fUl15TJrTpew",
+    authDomain: "gen-lang-client-0116991831.firebaseapp.com",
+    projectId: "gen-lang-client-0116991831",
+    storageBucket: "gen-lang-client-0116991831.firebasestorage.app",
+    messagingSenderId: "349182924142",
+    appId: "1:349182924142:web:172aef648a8db78019dd7f",
+    measurementId: "G-SDF0BWL770"
 };
 
 let app, db;
